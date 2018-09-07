ee1033686a1e22bb014ceab85deac6a27a4b540c
へーさん <hira9603859504@gmail.com>
2018-09-07 12:48:45 +0900

    pushAndTopテスト書いた

--- test/stack.py
+++ test/stack.py
@@ -2,6 +2,10 @@ import unittest
 from stack import Stack
 
 class TestStack(unittest.TestCase):
+    def __init__(self):
+        self.stack = Stack()
     def testCreate(self):
-        stack = Stack()
-        self.assertTrue(stack.isEmpty())
+        self.assertTrue(self.stack.isEmpty())
+    def testPushAndTop(self):
+        self.stack.push(1)
+        self.assertEqual(1, self.stack.top())
